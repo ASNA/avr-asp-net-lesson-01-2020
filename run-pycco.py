43277cb3b0f2e42b4eaef3ba90a75d4ecf14eff3
Roger Pence <roger.pence@gmail.com>
2020-03-05 16:55:17 -0600

    add index.html to docs folder

--- run-pycco.py
+++ run-pycco.py
@@ -11,12 +11,20 @@ def delete_folder_contents(folder):
             os.unlink(os.path.join(root, f))
         for d in dirs:
             shutil.rmtree(os.path.join(root, d))
+
+def write_directory(files):
+    template = '<a href="{url}">{filename}</a>'
+    for file in files: 
+
            
 searches = ('**/*.vr', 'global.asax', '**/*.aspx', '*.aspx', 'web.config')
 
 files = []
 for search in searches:
     files.extend(glob.glob(search, recursive=True)) 
+
+print(files)    
+exit()
 
 delete_folder_contents('./docs')
 
